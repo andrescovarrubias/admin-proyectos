1d3f8650786a7565a47a3beb3a02ea5f3ad0fb2c
Andres Alejandro Sañudo Covarrubias <asanudo@imca.local>
2020-10-31 12:54:16 -0700

    login

--- cliente/src/components/auth/Login.jsx
+++ cliente/src/components/auth/Login.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 
 const Login = () => {
+
+     // state para iniciar sesion
+     const [usuario, guardarUsuario] = useState ({
+          email:'',
+          password:''
+     });
+
+     // extraer el usuario
+     const {email, password} = usuario;
+
+     const onChange = e => {
+          guardarUsuario({
+               ...usuario,
+               [e.target.name] : e.target.value
+          })
+     }
+
      return ( 
-          <h1>desde login</h1>
+          <div className="form-usuario">
+               <div className="contenedor-form sombra-dark">
+                    <h1>Iniciar Sesión</h1>
+                    <form>
+                         <div className="campo-form">
+                              <label htmlFor="email">Usuario</label>
+                              <input 
+                                   type="email"
+                                   id="email"
+                                   name="email"
+                                   placeholder="Correo Electrónico"
+                                   autocomplete="off"
+                                   required
+                                   value={email}
+                                   onChange={onChange}     
+                              />
+                         </div>
+                         <div className="campo-form">
+                              <label htmlFor="password">Contraseña</label>
+                              <input 
+                                   type="password"
+                                   id="password"
+                                   name="password"
+                                   placeholder="contraseña"
+                                   autocomplete="off"
+                                   value={password}
+                                   required
+                                   onChange={onChange}     
+                              />
+                         </div>
+
+                         <div className="campo-form">
+                              <input type="submit" className=" btn btn-primario btn-block" value="Iniciar Sesión"/>
+                         </div>
+                    </form>
+               </div>
+          </div>
       );
 }
  
