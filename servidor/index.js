1bc1d8b35a0f089cbae5dbda1a3d499dac65d520
Andres Alejandro Sañudo Covarrubias <asanudo@imca.local>
2020-11-11 09:31:33 -0700

    conexion a la base de datos, mongodb, archivo db.js con lafuncion de conexion

--- servidor/index.js
+++ servidor/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
+const conectarDB = require("./config/db");
 
 // Creando el servidor
 const app = express();
+
+// Conectar con la db
+conectarDB();
 
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
 
 // Arrancar la app
 app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
-})
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
