adc152cafcb3e6a16dfb590fd5e1df7077bac80a
andrescovarrubias <45583739+andrescovarrubias@users.noreply.github.com>
2020-11-10 23:36:33 -0700

    Eliminacion de archivos, creando servidor express, instalacion de dependencias, gitignore, variables

---
+++ servidor/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Creando el servidor
+const app = express();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+})
