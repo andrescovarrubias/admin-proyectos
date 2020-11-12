2fc7c0786d1250e2dbedf62b45d2358c01469c8f
Andres Alejandro Sañudo Covarrubias <asanudo@imca.local>
2020-11-11 17:22:20 -0700

    crear, agregar usuario

---
+++ servidor/controllers/usuarioController.js
@@ -0,0 +1,19 @@
+const Usuario = require("../models/Usuario");
+
+exports.crearUsuario = async (req, res) => {
+  try {
+    let usuario;
+
+    // Crea el nuevo usuario
+    usuario = new Usuario(req.body);
+
+    // Guarda el nuevo usuario
+    await usuario.save();
+
+    // Mensaje de confirmacion
+    res.send("Usuario creado correctamente");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Hubo un error");
+  }
+};
--- servidor/index.js
+++ servidor/index.js
@@ -7,8 +7,14 @@ const app = express();
 // Conectar con la db
 conectarDB();
 
+// Habilitar express.Json
+app.use(express.json({ extended: true }));
+
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
+
+// Importar rutas
+app.use("/api/usuarios", require("./routes/usuarios"));
 
 // Arrancar la app
 app.listen(PORT, () => {
